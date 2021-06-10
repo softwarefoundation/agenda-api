9667b8067b52bc21461a6caa71f391c112904e1a
Dherkyan Ribeiro <dherkyan@gmail.com>
2021-06-10 05:34:29 -0300

    Mapeando a classe Anotacao

--- src/main/java/com/agenda/entity/Anotacao.java
+++ src/main/java/com/agenda/entity/Anotacao.java
@@ -1,19 +1,30 @@
 package com.agenda.entity;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Data
+@Entity
+@Table(name = "tb01_anotacao")
 public class Anotacao {
 
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "titulo", length = 30)
     private String titulo;
+
+    @Column(name = "texto", length = 100)
     private String texto;
+    
+    @Column(name = "data_evento")
     private LocalDateTime dataEvento;
+
+    @Column(name = "data_cadastro")
     private LocalDateTime dataCadastro;
 
 }
